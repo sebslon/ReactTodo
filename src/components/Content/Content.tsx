14dc582ebf6089f615483be0cb54b660eea545c6
sebslon <sebastesterprogram@gmail.com>
2021-03-15 19:50:53 +0100

    Create TasksList component

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,22 +1,20 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { default as bemCssModules } from 'bem-css-modules';
 import { default as ContentStyles } from './Content.module.scss';
-import Task from '../Task/Task';
 import AddTask from '../AddTask/AddTask';
+import TasksList from '../TasksList/TasksList';
 
 const style = bemCssModules(ContentStyles);
 
-const taskList = (): JSX.Element => <div>Task List</div>
-
 const Content: React.FC = () => {
   return (
-    <Switch>
-      <Route component={AddTask} path="/addtask" />
-      <Route component={taskList} path="/:id" />
-      <Route component={Task} path="/" />
-      <Redirect from="*" to="/todo" />
-    </Switch>
+      <Switch>
+        <Route component={AddTask} path="/addtask" />
+        <Route component={TasksList} path="/:id" />
+        <Route component={TasksList} path="/" />
+        <Redirect from="*" to="/todo" />
+      </Switch>
   );
 }
- 
+
 export default Content;
---
+++ src/components/TasksList/TasksList.tsx
@@ -0,0 +1,27 @@
+import { default as bemCssModules } from 'bem-css-modules';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { DoneTask } from '../../@types/doneTasks.types';
+import { TodoTask } from '../../@types/todoTasks.types';
+import { RootState } from '../../reducers/rootReducer';
+import Task from '../Task/Task';
+import { default as TasksListStyles } from './TasksList.module.scss';
+
+const style = bemCssModules(TasksListStyles);
+
+type Task = TodoTask | DoneTask;
+
+const TasksList: React.FC = () => {
+  const id = useParams();
+  const tasks = useSelector((state: RootState) => id === 'done' ? state.done : state.todo)
+
+  return (
+    <div className={style()}>
+      {(tasks as Task[]).map(task => (
+        <Task key={task.id} {...task} />
+      ))}
+    </div>
+  );
+}
+ 
+export default TasksList;
